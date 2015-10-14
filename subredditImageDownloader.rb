45cd8e809d0fbff658c900c0387d71d19abe8673
Joel <joelelliott819@gmail.com>
2015-10-14 02:44:30 -0500

    minor changes and comments

--- subredditImageDownloader.rb
+++ subredditImageDownloader.rb
@@ -2,16 +2,23 @@ require 'open-uri'
 require 'redditkit'
 
 def download_imgur(url, filename)
-  image = open("#{url}.jpg").read
-  File.write("#{filename}.jpg", image)
+  image = open("#{url}").read
+  File.write("./downloaded/#{filename}", image)
 end
 
 client = RedditKit::Client.new
-posts = client.links 'aww', :limit => '25'
+client.user_agent = 'subreddit Image Downloader'
+#p client.user_agent
+
+subreddit = 'aww'
+#set this to change the subreddit
+posts = client.links subreddit, :limit => '25'
+#acquire posts from previously specified subreddit
 posts.each.with_index(0) do |r, i|
   if r.image_link?
     puts "Downloading #{i}"
-    download_imgur r.url, r.id
+    download_imgur r.url, "#{subreddit}#{i}"
+    #downloads each file and names it after the subreddit
   end
   sleep(2)
 end
