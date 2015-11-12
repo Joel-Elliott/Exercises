358a02f380072e43b23429a3a5feefbdfec99216
Joel <joelelliott819@gmail.com>
2015-11-12 02:40:56 -0600

    changed gw2mailer.pl to use argv instead of hardcoded email/pass

--- perl/gw2mailer.pl
+++ perl/gw2mailer.pl
@@ -6,19 +6,21 @@ use warnings;
 use Email::Send::SMTP::Gmail;
 use LWP::Simple;
 
+my ($username, $password) = @ARGV;
+my $api = 'INSERT API KEY HERE'
 my $url = "https://api.guildwars2.com/v2/";
-my $api_key = "?access_token=INSERT API KEY HERE";
+my $api_key = "?access_token=$api";
 
-my $content = get($url."/account".$api_key);
-die ':(' unless defined $content;
+my $account = get($url."/account".$api_key);
+my $characters = get ($url."/characters/".$api_key);
+die ':(' unless defined $account;
 
-#say $content;
-
+say $age;
 my $mail=Email::Send::SMTP::Gmail->new( -smtp=>'smtp.gmail.com',
-                                                 -login=>'username@gmail.com',
-                                                 -pass=>'password',
+                                                 -login=>"$username".'@gmail.com',
+                                                 -pass=>"$password",
                                                  -port=>587);
 
-$mail->send(-to=>'username@gmail.com', -subject=>'GW2 Account info!', -body=>"$content",);
+$mail->send(-to=>"$username".'@gmail.com', -subject=>'GW2 Account info!', -body=>"$account \n\n $characters",);
 
 $mail->bye;
