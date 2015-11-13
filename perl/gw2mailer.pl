7311a2338fe36ba62aeba49b09c2e3429bce71ee
Joel <joelelliott819@gmail.com>
2015-11-12 19:31:47 -0600

    small fix + now sends character age as string

--- perl/gw2mailer.pl
+++ perl/gw2mailer.pl
@@ -5,22 +5,30 @@ use warnings;
 
 use Email::Send::SMTP::Gmail;
 use LWP::Simple;
+use JSON::MaybeXS ':all';
+#use Data::Dumper;
 
 my ($username, $password) = @ARGV;
-my $api = 'INSERT API KEY HERE'
+my $api = 'INSERT API KEY HERE';
 my $url = "https://api.guildwars2.com/v2/";
 my $api_key = "?access_token=$api";
 
 my $account = get($url."/account".$api_key);
-my $characters = get ($url."/characters/".$api_key);
+my $characters = get ($url."/characters".$api_key);
+my $characterInfo = get ($url."/characters/Vekk%20Bond".$api_key);
+
+my %characterAge = %{decode_json($characterInfo)};
+
+#print $characterAge{age};
+#print Dumper $characterAge;
+
 die ':(' unless defined $account;
 
-say $age;
 my $mail=Email::Send::SMTP::Gmail->new( -smtp=>'smtp.gmail.com',
                                                  -login=>"$username".'@gmail.com',
                                                  -pass=>"$password",
                                                  -port=>587);
 
-$mail->send(-to=>"$username".'@gmail.com', -subject=>'GW2 Account info!', -body=>"$account \n\n $characters",);
+$mail->send(-to=>"$username".'@gmail.com', -subject=>'GW2 Account info!', -body=>"$account \n\n $characters\n\n$characterInfo\n\n$characterAge{age}",);
 
 $mail->bye;
