2c67536c8d37c92e6c1b5f5c867f4371168989d0
Joel <joelelliott819@gmail.com>
2015-11-13 01:58:21 -0600

    added a field to the email of the time played since the script was last ran.

--- perl/gw2mailer.pl
+++ perl/gw2mailer.pl
@@ -8,30 +8,44 @@ use LWP::Simple;
 use JSON::MaybeXS ':all';
 #use Data::Dumper;
 
+#get username and password from command line #TODO: This could be improved
 my ($username, $password) = @ARGV;
-my $api = 'INSERT API KEY HERE';
+my $api = 'INSERT API KEY HERE';  #Paste your API key here.  TODO: Add a prompt if no key provided by user
 my $url = "https://api.guildwars2.com/v2/";
 my $api_key = "?access_token=$api";
 
-my $account = get($url."/account".$api_key);
-my $characters = get ($url."/characters".$api_key);
-my $characterInfoJSON = get ($url."/characters/Vekk%20Bond".$api_key);
+#my $account = get($url."/account".$api_key);
+#die ':(' unless defined $account;
 
-my %characterInfo = %{decode_json($characterInfoJSON)};
+#my $characters = get ($url."/characters".$api_key);
+my $characterInfoJSON = get ($url."/characters/Vekk%20Bond".$api_key);    #you would change this line to hold your characters name with spaces replaced with %20
+die ':(' unless defined $characterInfoJSON;
+
+my %characterInfo = %{decode_json($characterInfoJSON)}; #gets a hash of all character info from the JSON received from the API
 my $characterAgeSeconds = $characterInfo{age};
-my $days = (gmtime($characterAgeSeconds))[3] - 1;
-#print "$days\n";
-#
-my $characterAgeFormatted .= sprintf ("%02dd:%02dh:%02dm:%02ds\n",$days,(gmtime($characterAgeSeconds))[2,1,0]);
+my $days = (gmtime($characterAgeSeconds))[3] - 1; #Days was showing up 1 too high in testing
 
-#print "$characterAgeFormatted";
-die ':(' unless defined $account;
+my $characterAgeFormatted .= sprintf ("%02dd:%02dh:%02dm:%02ds\n",$days,(gmtime($characterAgeSeconds))[2,1,0]); #formats the character's age in a dd:hh:mm:ss format
 
+#open the file and store the contents in a variable. the contents are the time from the last time the script was ran.
+my $filename = 'timePlayed.txt';
+
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+my $previousTime = <$fh>;
+close $fh;
+#calculate the difference between the time it was ran now and previously
+my $timeDifference = $characterAgeSeconds - $previousTime;
+
+open($fh, '>:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+print $fh "$characterAgeSeconds";
+close $fh;
+
+#build the email object & login to the gmail smtp server through TLS required port 587
 my $mail=Email::Send::SMTP::Gmail->new(-smtp=>'smtp.gmail.com',
                                        -login=>"$username".'@gmail.com',
                                        -pass=>"$password",
                                        -port=>587);
-
-$mail->send(-to=>"$username".'@gmail.com', -subject=>'GW2 Character Age !', -body=>"You have played this character for $characterAgeFormatted\n",);
+#send the email to self
+$mail->send(-to=>"$username".'@gmail.com', -subject=>'GW2 Character Age !', -body=>"You have played this character for $characterAgeFormatted total.\nYou have played for $timeDifference since the last time this script was ran.\n",);
 
 $mail->bye;
